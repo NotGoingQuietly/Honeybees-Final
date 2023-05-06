897a1cdeb58ae9d9e49f2854fc389bf0eaf89766
dgkeating <dgkeating@gmail.com>
2023-05-06 10:01:49 -0400

    Update colony.py
    Final app with updated comment sections

--- colony.py
+++ colony.py
@@ -27,7 +27,7 @@ geojson_data = load_geojson_data()
 
 st.title("Honeybee Colony Inventory and Colony Loss")
 #  
-# Sample DataFrame (use your own data)
+# Sample DataFrame 
 data = pd.read_csv('data/honeybee_colonies_2023_03_26.csv')
 data = data[['Year','Period','State' ,'Inventory','Colony_Loss','Colony_loss_pct']].copy()
 data['Colony_loss_pct'] = pd.to_numeric(data['Colony_loss_pct'], errors='coerce')
@@ -38,7 +38,6 @@ data_without_2022 = data[data['Year'] != 2022]
 data_grouped = data_without_2022.groupby(["Year", "State"]).agg({"Inventory": "mean", "Colony_Loss": "mean", "Colony_loss_pct": "mean"}).reset_index()
 
 data3 = data_without_2022.sort_values(by=['State','Year'])
-#data3.set_index('Period', inplace=True)
 def introduction():
     
     st.header("Did you know that honeybees are critical to food production in the United States? ")
@@ -49,7 +48,7 @@ def introduction():
     # Define columns
     col1, col2, col3 = st.columns([2,3,1], gap="small")
 
-    # Add content to the columns
+    # Add image and narrative for introduction
     with col1:
         st.image("data/honeybee.jpg", use_column_width=True)
 
@@ -59,9 +58,11 @@ def introduction():
     with col3:
         st.write("""
         """)
+    #Added information about data source
     st.subheader("About the Data Set")
     st.markdown("Data for Nevada, New Hampshire, Delaware and Rhode Island was not reported. Data was aggregated to provide average total hives, average hive loss, and average percent hive loss")
     st.markdown("[Data was obtained from Honeybee Surveys and Reports from the US Deparment of Agriculture ](https://www.nass.usda.gov/Surveys/Guide_to_NASS_Surveys/Bee_and_Honey/)")
+#Created interactive map
 def interactive_map():
     st.header("Interactive Map")
     st.markdown("Map is color coded based on percent colony loss")
@@ -80,7 +81,7 @@ def interactive_map():
      geojson_data = json.load(f)
 
 
-    # Merge the year_data with the GeoJSON data
+    # Merge the data frame  with the GeoJSON data based on state from dataframe and name from GeoJSON
     for feature in geojson_data["features"]:
         state_data = year_data.loc[year_data["State"] == feature["properties"]["name"]]
         if not state_data.empty:
@@ -124,7 +125,7 @@ def interactive_map():
 
     # Display the map
     folium_static(m)
-
+#Create line to compare states for colony inventory and percent colony loss
 def line_chart():
     st.subheader("Compare honeybee average hive inventory and average percent hive loss from 2015-2021")
 
@@ -141,7 +142,7 @@ def line_chart():
         st.warning("Please select no more than 3 states.")
         return
 
-    # Filter out the year 2022
+    # Filter out the year 2022 since reporting data is only for first and second quarter
     filtered_data = data3.loc[data3['Year'] != 2022]
 
     if len(states) > 0:
@@ -158,7 +159,7 @@ def line_chart():
         if selected_variable == 'Colony_loss_pct':
             y_axis_title += ' (%)'
 
-        # Create a line chart comparing the average inventory and colony loss by year for the selected states
+        # Create a line chart comparing the average inventory and colony loss by year for the selected states. Add tool tip with completed information
         chart = alt.Chart(aggregated_data).mark_line().encode(
             x=alt.X('Year:N', axis=alt.Axis(title='Year')),
             y=alt.Y(f'mean({selected_variable}):Q', axis=alt.Axis(title=y_axis_title, format='.0f' if variable == 'Inventory' else '.2%', tickCount=5)),
@@ -170,6 +171,7 @@ def line_chart():
                 alt.Tooltip('mean(Colony_Loss):Q', title='Average Colony Loss', format=',.0f'),
                 alt.Tooltip('mean(Colony_loss_pct):Q', title='Average Percent Colony Loss', format='.2%')
             ]
+        # Adjust the widthe and height of the line chart
         ).properties(
             width=800,
             height=400
